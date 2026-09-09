29ef66e6ad94a3320a419edef3b25e4c409c27e1
sloth0162-cmyk <sloth0162@gmail.com>
2026-09-09 12:09:38 +0530

    auth production bug fix

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,8 +1,7 @@
-/// <reference types="vite/types/importMeta.d.ts" />
-import { createBrowserClient } from '@supabase/ssr'
+import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 export function createClient() {
-  return createBrowserClient(
+  return createSupabaseClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
   );
